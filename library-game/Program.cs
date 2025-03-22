a588d50510ade7d4ee4a331c5893950efd073f03
Vihr_Kraft <sanefedov2009@gmail.com>
2025-03-22 16:01:10 +0700

    Добавил код

---
+++ library-game/Program.cs
@@ -0,0 +1,24 @@
+﻿string filePath = "GameLibrary.txt";
+Console.Write("Сколько игр хотите добавить? ");
+int gamesCount = int.Parse(Console.ReadLine());
+for (int num = 1; num<=gamesCount; num++) {
+    Console.Write("\nВведите жанр игры: ");
+    string genre = Console.ReadLine();
+    Console.Write("Введите название игры: ");
+    string gameName = Console.ReadLine();
+    Console.Write("Введите название студии разработчика: ");
+    string studioName = Console.ReadLine();
+    string game = $"Игра {genre}: {{Название: {gameName}, Разработчик: {studioName}}}";
+    File.AppendAllText(filePath, game + "\n"); 
+}
+Console.WriteLine("\nИгры добавлены в бибилиотеку!");
+if (File.Exists(filePath)) {
+    string[] lines = File.ReadAllLines(filePath);
+    Console.WriteLine("\nОбновленный текст файла:");
+    foreach (string line in lines) {
+        Console.WriteLine(line);
+    }
+}
+else {
+    Console.WriteLine("Файл не найден.");
+}
